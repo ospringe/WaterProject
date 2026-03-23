a9bad9471648eb7c97d1014b5472ee59d08bb18a
Olivia Springer <oliviagspringer@gmail.com>
2026-03-22 21:30:04 -0600

    Phase 03

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
-import './App.css'
-import ProjectList from './ProjectList'
+import { useState } from 'react';
+import './App.css';
+import CategoryFilter from './CategoryFilter';
+import ProjectList from './ProjectList';
+import WelcomeBand from './WelcomeBand';
 
 function App() {
-  
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+    
   return (
     <>
-      <ProjectList />
+      <div className="container mt-4">
+        <div className="row bg-primary text-white">
+          <WelcomeBand />
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <CategoryFilter selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories}/>
+          </div>
+          <div className="col-md-9">
+            <ProjectList selectedCategories={selectedCategories}/>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
